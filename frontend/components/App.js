197a1be89b1f4b2bd4fbb4e78bfde26da9be37a1
Courtney Johnson <cjandy13@gmail.com>
2025-01-16 17:06:43 -0600

    Project FINISHED

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,12 +7,39 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  
+  
+  
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    Promise.all([axios.get(urlPeople), axios.get(urlPlanets)])
+      .then(([peopleRes, planetsRes]) => {
+        setPeople(peopleRes.data);
+        setPlanets(planetsRes.data);
+        //console.log(peopleRes, planetsRes)
+      })
+      .catch((err) => {
+        console.error('Error fetching data', err);
+        
+      })
+  }, [])
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {people.map((person, index) => {
+        const planet = planets.find((planet) => planet.id === person.homeworld)
+       
+        return(
+          
+          <Character person={person} planet={planet}/>
+          
+        )
+      })}
+      
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ person, planet }) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+
+  const [rendered, setRendered] = useState(false);
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggle = (onClick) => {
+    setRendered(!rendered);
+    console.log('planet rendered')
+  }
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card"onClick={toggle}>
+      <h3 className="character-name" >
+        {person.name}
+      </h3>
+
+      {rendered && planet && (
+        <p>
+          Planet:  <span className="character-planet">{planet.name}</span>
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
 export default Character
